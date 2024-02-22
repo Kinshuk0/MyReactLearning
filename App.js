cf23fe99160bb88d6a912b76c161b8a9fb657147
Kinshuk Dwivedi <007kinshuk@gmail.com>
2024-02-22 15:18:41 +0530

    first milestone

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
 const heading = React.createElement(
   "h1",
   { className: "hitme" },
-  "Hey! this is used by React CDN link"
+  "Hey! this is used by React CDN link this is save check "
 );
 // console.log(heading);
 const root = ReactDOM.createRoot(document.getElementById("root"));
