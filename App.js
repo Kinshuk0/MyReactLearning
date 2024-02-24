559ebc87ce0a4a540e125c9c21f7af21b368ae91
Kinshuk Dwivedi <007kinshuk@gmail.com>
2024-02-24 14:07:25 +0530

    Started with swiggy

--- App.js
+++ App.js
@@ -1,15 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Y = () => {
-  return <div className="h1">This is Kinshuk</div>;
-};
-const z = <h1>I am text inside Y</h1>;
-const X = () => {
-  return (
-    <Y>
-    </Y>
-  );
-};
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<X />);
+root.render();
