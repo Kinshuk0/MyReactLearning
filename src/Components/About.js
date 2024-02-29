f5edb69f16cb00edea747d903768feb3ab4d1c6e
Kinshuk Dwivedi <007kinshuk@gmail.com>
2024-02-29 19:10:20 +0530

    routes

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const About = () => {
-  return <div>This is the about page</div>;
+  return (
+    <div>
+      <h1>This is Swiggy Clone created by Kinshuk Dwivedi</h1>
+      <Link to="/Contact">Contact me here</Link>
+    </div>
+  );
 };
-
 export default About;
